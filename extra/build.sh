5717c5830cb3e3569a6101252c34af0be4ef87ea
Daniel Cliche <dancliche@gmail.com>
2025-09-25 16:21:24 -0400

    Example code now using latest toolchain

--- extra/build.sh
+++ extra/build.sh
@@ -1,4 +1,4 @@
-build/intermediate/cpp-0-strip/cpp.oe main.c -o main.ci
-build/intermediate/cci-0-omc/cci.oe main.ci -o main.os
+build/intermediate/cpp-1-omc/cpp.oe main.c -o main.i
+build/intermediate/cci-0-omc/cci.oe main.i -o main.os
 build/intermediate/as-1-compound/as.oe main.os -o main.oo
-build/intermediate/ld-0-global/ld.oe build/intermediate/libc-0-oo/libc.oa main.oo -o main.oe
+build/intermediate/ld-1-omc/ld.oe build/intermediate/libc-1-omc/libc.oa main.oo -o main.oe
