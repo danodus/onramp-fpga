5e2194f7a5414d9231e5334defe63459e7a146c7
Daniel Cliche <dancliche@gmail.com>
2025-10-17 14:26:04 -0400

    Add visual editor del, home, end, pgup and pgdn keys

--- src/ed/ed.c
+++ src/ed/ed.c
@@ -20,7 +20,12 @@ enum editor_key {
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
-    ARROW_DOWN
+    ARROW_DOWN,
+    DEL_KEY,
+    HOME_KEY,
+    END_KEY,
+    PAGE_UP,
+    PAGE_DOWN
 };
 
 /*** data ***/
@@ -61,11 +66,33 @@ int editor_read_key() {
         while (read(STDIN_FILENO, &seq[1], 1) != 1);
 
         if (seq[0] == '[') {
+            if (seq[1] >= '0' && seq[1] <= '9') {
+                while (read(STDIN_FILENO, &seq[2], 1) != 1);
+                if (seq[2] == '~') {
+                    switch (seq[1]) {
+                        case '1': return HOME_KEY;
+                        case '3': return DEL_KEY;
+                        case '4': return END_KEY;
+                        case '5': return PAGE_UP;
+                        case '6': return PAGE_DOWN;
+                        case '7': return HOME_KEY;
+                        case '8': return END_KEY;
+                    }
+                }
+            } else {
+                switch (seq[1]) {
+                    case 'A': return ARROW_UP;
+                    case 'B': return ARROW_DOWN;
+                    case 'C': return ARROW_RIGHT;
+                    case 'D': return ARROW_LEFT;
+                    case 'H': return HOME_KEY;
+                    case 'F': return END_KEY;
+                }
+            }
+        } else if (seq[0] == 'O') {
             switch (seq[1]) {
-                case 'A': return ARROW_UP;
-                case 'B': return ARROW_DOWN;
-                case 'C': return ARROW_RIGHT;
-                case 'D': return ARROW_LEFT;
+                case 'H': return HOME_KEY;
+                case 'F': return END_KEY;
             }
         }
 
@@ -194,16 +221,20 @@ void editor_refresh_screen() {
 void editor_move_cursor(int key) {
     switch (key) {
         case ARROW_LEFT:
-            E.cx--;
+            if (E.cx != 0)
+                E.cx--;
             break;
         case ARROW_RIGHT:
-            E.cx++;
+            if (E.cx != E.screen_cols - 1)
+                E.cx++;
             break;
         case ARROW_UP:
-            E.cy--;
+            if (E.cy != 0)
+                E.cy--;
             break;
         case ARROW_DOWN:
-            E.cy++;
+            if (E.cy != E.screen_rows - 1)
+                E.cy++;
             break;
     }
 }
@@ -217,6 +248,24 @@ void editor_process_keypress() {
             write_all(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
+
+        case HOME_KEY:
+            E.cx = 0;
+            break;
+
+        case END_KEY:
+            E.cx = E.screen_cols - 1;
+            break;
+
+        case PAGE_UP:
+        case PAGE_DOWN:
+            {
+                int times = E.screen_rows;
+                while (times--)
+                    editor_move_cursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+            }
+            break;
+
         case ARROW_UP:
         case ARROW_DOWN:
         case ARROW_LEFT:
