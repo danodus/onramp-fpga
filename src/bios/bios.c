9bbb2c1a19b612ab2868be89325a6ff4c6366d89
Daniel Cliche <dancliche@gmail.com>
2026-02-18 14:46:22 -0500

    Unused function removed

--- src/bios/bios.c
+++ src/bios/bios.c
@@ -33,30 +33,6 @@ static void receive(void) {
     print("Program received.\n");
 }
 
-// ref: https://stackoverflow.com/questions/8257714/how-to-convert-an-int-to-string-in-c
-static char* uitoa(unsigned int value, char* result, int base)
-{
-    // check that the base if valid
-    if (base < 2 || base > 36) { *result = '\0'; return result; }
-
-    char* ptr = result, *ptr1 = result, tmp_char;
-    int tmp_value;
-
-    for (int i = 0; i < 8; ++i) {
-        tmp_value = value;
-        value /= base;
-        *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
-    }
-
-    *ptr-- = '\0';
-    while(ptr1 < ptr) {
-        tmp_char = *ptr;
-        *ptr--= *ptr1;
-        *ptr1++ = tmp_char;
-    }
-    return result;
-}
-
 int main(void) {
 
     // TODO: Is there a way to use static assert instead?
